b46685fddd0b5b3e734b6f088b42265b338d5a6f
Mayadamohamed1 <111245998+Mayadamohamed1@users.noreply.github.com>
2024-03-03 19:17:52 +0200

    Update main.js

--- main.js
+++ main.js
@@ -7,46 +7,34 @@ function winner(){
 if(square[1]==square[2]&& square[2]==square[3]&&square[2]!=''){
 
     title.innerHTML=`${square[1]} Winner`;
-    document.getElementById('item'+1)=document.getElementById('color');
-    document.getElementById('item'+2)=document.getElementById('color');
-    document.getElementById('item'+3)=document.getElementById('color');
+    alert('player' +square[1] +' wins the game');
+
 
 }else if(square[4]==square[5]&& square[5]==square[6]&&square[5]!=''){
     title.innerHTML=`${square[4]} Winner`;
+    alert('player' +square[4] +' wins the game');
 
-    document.getElementById('item'+4)=document.getElementById('color');
-    document.getElementById('item'+5)=document.getElementById('color');
-    document.getElementById('item'+6)=document.getElementById('color');
+    
 }else if(square[7]==square[8]&& square[8]==square[9]&&square[7]!=''){
     title.innerHTML=`${square[7]} Winner`;
-    document.getElementById('item'+7)=document.getElementById('color');
-    document.getElementById('item'+8)=document.getElementById('color');
-    document.getElementById('item'+9)=document.getElementById('color');
+    alert('player' +square[7] +' wins the game');
+    
 }else if(square[1]==square[4]&& square[4]==square[7]&&square[1]!=''){
     title.innerHTML=`${square[1]} Winner`;
-    document.getElementById('item'+1)=document.getElementById('color');
-    document.getElementById('item'+4)=document.getElementById('color');
-    document.getElementById('item'+7)=document.getElementById('color');
+    alert('player' +square[1] +' wins the game');
+    
 }else if(square[2]==square[5]&& square[5]==square[8]&&square[5]!=''){
     title.innerHTML=`${square[2]} Winner`;
-    document.getElementById('item'+2)=document.getElementById('color');
-    document.getElementById('item'+5)=document.getElementById('color');
-    document.getElementById('item'+8)=document.getElementById('color');
+    alert('player' +square[2] +' wins the game');
 }else if(square[3]==square[6]&& square[6]==square[9]&&square[9]!=''){
     title.innerHTML=`${square[3]} Winner`;
-    document.getElementById('item'+3)=document.getElementById('color');
-    document.getElementById('item'+6)=document.getElementById('color');
-    document.getElementById('item'+9)=document.getElementById('color');
+    alert('player' +square[3] +' wins the game');
 }else if(square[1]==square[5]&& square[5]==square[9]&&square[1]!=''){
     title.innerHTML=`${square[1]} Winner`;
-    document.getElementById('item'+1)=document.getElementById('color');
-    document.getElementById('item'+5)=document.getElementById('color');
-    document.getElementById('item'+9)=document.getElementById('color');
+    alert('player' +square[1] +' wins the game');
 }else if(square[3]==square[5]&& square[5]==square[7]&&square[3]!=''){
     title.innerHTML=`${square[3]} Winner`;
-    document.getElementById('item'+3)=document.getElementById('color');
-    document.getElementById('item'+5)=document.getElementById('color');
-    document.getElementById('item'+7)=document.getElementById('color');
+    alert('player' +square[3] +' wins the game');
 }
 
 }
